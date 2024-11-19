e18951ce4f97dbf4a03479e5ad81f8b7afc2ca40
Mouse <farrasaddar@gmail.com>
2024-11-19 16:41:50 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function buy(productName, price) {
+    alert(`Terima kasih sudah membeli ${productName} Dengan harga Rp${price}k`);
+}
+
+function error(error) {
+    alert('Mohon maaf fitur ${error}');
+}
